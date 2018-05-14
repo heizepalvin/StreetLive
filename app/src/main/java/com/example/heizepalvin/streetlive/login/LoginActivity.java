078bf0632bf56a9e6049f4150d6ed3ddfd3dc404
heizepalvin <palvin1993@naver.com>
2018-05-14 20:49:03 +0900

    20180514

--- app/src/main/java/com/example/heizepalvin/streetlive/login/LoginActivity.java
+++ app/src/main/java/com/example/heizepalvin/streetlive/login/LoginActivity.java
@@ -1,17 +1,23 @@
 package com.example.heizepalvin.streetlive.login;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.heizepalvin.streetlive.R;
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
 import com.tsengvn.typekit.Typekit;
 import com.tsengvn.typekit.TypekitContextWrapper;
 
@@ -42,11 +48,55 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.loginActivityNaverBtn)
     Button loginActivityNaverBtn;
 
+    //네이버 연동 로그인 시작
+
+    private OAuthLogin naverOAuthLoginMoudle;
+    private Context naverLoginContext;
+
+    private void setNaverLogin(){
+        naverOAuthLoginMoudle = OAuthLogin.getInstance();
+        naverOAuthLoginMoudle.init(this,"OPwHEtx_WJdiSFrZowv3","gCO1W3I8VS","StreetLive");
+        loginActivityNaverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                naverOAuthLoginMoudle.startOauthLoginActivity(LoginActivity.this,naverOAuthLoginHandler);
+            }
+        });
+    }
+
+    //네이버 로그인 핸들러 시작
+
+    @SuppressLint("HandlerLeak")
+    private OAuthLoginHandler naverOAuthLoginHandler = new OAuthLoginHandler() {
+
+        @Override
+        public void run(boolean success) {
+            if(success){
+                String accessToken = naverOAuthLoginMoudle.getAccessToken(naverLoginContext);
+                String refreshToken = naverOAuthLoginMoudle.getRefreshToken(naverLoginContext);
+                long expiresAt = naverOAuthLoginMoudle.getExpiresAt(naverLoginContext);
+                String tokenType = naverOAuthLoginMoudle.getTokenType(naverLoginContext);
+                Log.d("naverAccessToken",accessToken);
+                Log.d("naverRefreshToken",refreshToken);
+                Log.d("naverExpiresAt",expiresAt+"");
+                Log.d("naverTokenType",tokenType);
+            } else {
+                Log.d("naverLoginFail","실패요");
+                String errorCode = naverOAuthLoginMoudle.getLastErrorCode(naverLoginContext).getCode();
+                String errorDesc = naverOAuthLoginMoudle.getLastErrorDesc(naverLoginContext);
+                Toast.makeText(naverLoginContext, "errorCode:" + errorCode + ", errorDesc:"+ errorDesc
+                        , Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+    //네이버 로그인 핸들러 끝
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         ButterKnife.bind(this);
+        setNaverLogin();
 
         // ID 입력하는 EditText 포커스 이벤트 리스너
 
